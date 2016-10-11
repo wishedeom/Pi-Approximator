a22c1ac629836ac491e2f357ee4c1108edbc435f
Michael Deom <wishedeom@gmail.com>
2016-10-11 18:49:29 -0400

    Ayyyy

--- multi_pi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for n in 2 4 6 8 10 12 14 16 18 20
-do
-    for i in {1..10}
-    do
-        echo "$(mpirun -np $n ./bin/pi_approx | cut -d ' ' -f 8)"  >> ./"out$n".txt
-    done
-done
--- serial_pi.sh
+++ serial_pi.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-> ./out/out01.txt
+> rm ~comp428/a1/out/*.txt
+
 for i in {1..10}
 do
-    echo $(./bin/pi_approx_serial | cut -f 5 -d ' ')  >> ./out/out01.txt
+    echo $(~comp428/a1/bin/pi_approx_serial | cut -f 5 -d ' ')  >> ~comp428/a1/out/out01.txt
 done
+
+for n in 3 5 7 9 11 13 15 17 19
+do
+    for i in {1..10}
+    do
+        echo "$(mpirun -np $n ~comp428/a1/bin/pi_approx | cut -d ' ' -f 8)"  >> "~comp428/a1/out/out$n".txt
+    done
+done
+
